304dcc5bb47d3f3f90d30a584e4104605e455c05
flapperz <flap.atyz@gmail.com>
2024-07-09 07:46:55 +0700

    Update workspace config

--- build-utils/cmake_slicer.sh
+++ build-utils/cmake_slicer.sh
@@ -6,3 +6,11 @@ cmake \
   /Users/flap/Source/Slicer
 
 # then do make -j6sq
+# 
+
+ arch -x86_64 /usr/local/bin/cmake \
+  -DCMAKE_OSX_DEPLOYMENT_TARGET:STRING=14.0 \
+  -DCMAKE_BUILD_TYPE:STRING=Release \
+-DCMAKE_OSX_ARCHITECTURES:STRING=x86_64 \
+  -DQt5_DIR:PATH=/Users/flap/Qt/5.15.2/clang_64/lib/cmake/Qt5 \
+/Users/flap/Source/Slicer
